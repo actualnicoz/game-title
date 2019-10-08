0063c8f4dc19f82eb9d620b620e20698be48e9c2
Nic <actualnicoz@protonmail.com>
2019-10-08 19:25:28 -0400

    style: reformatting
    Remove game loop from main.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "SDL.h"
 #include "renderer.h"
+#include "game.h"
 
 int main(){
   constexpr std::size_t kScreenWidth(640);
@@ -8,22 +9,8 @@ int main(){
   constexpr std::size_t kGridHeight(32);
   
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
+  renderer.UpdateWindowTitle();
+  Game game(kGridWidth, kGridHeight);
   
-  //create SDL event structure
-  SDL_Event event;
-  int gameOver = 0;
-
-  //game loop
-  while(!gameOver){
-    if(SDL_PollEvent(&event)){
-      //TODO: add score to window title
-      renderer.UpdateWindowTitle();
-      switch(event.type){
-      case SDL_QUIT:
-	gameOver = 1;
-	break;
-      }
-    }
-  }
   return 0;
 }
